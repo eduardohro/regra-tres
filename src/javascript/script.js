46b73e827cdb11b7a0411b15aaccca2a0389948d
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-03-06 16:57:15 -0300

    Create script

---
+++ src/javascript/script.js
@@ -0,0 +1,10 @@
+document.getElementById('calculate').addEventListener('click', function () {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const num3 = parseFloat(document.getElementById('num3').value);
+
+    let result = (num3 * num2) / num1;
+
+    document.getElementById('result').textContent = result;
+
+})
